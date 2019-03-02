7765578f3084f1540345b5d8f101e68c265541e6
ansushina <nastya.sushina@yandex.ru>
2019-03-02 18:19:31 +0300

    first version was added

---
+++ alg/alg1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+//using namespace std;
+
+int main()
+{
+    int n;
+    std::cin << n;
+    int A[n];
+    int B[n];
+
+    int imax = 0, jmax = 0, amax = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > a[amax])
+        {
+            if (a[i] + b[i] > a[imax] + b[imax])
+            {
+                s
+            }
+        }
+    }
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
