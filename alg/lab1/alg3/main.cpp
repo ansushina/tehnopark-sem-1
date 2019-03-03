3e308cc7bfabce5a7f2743e1b87313536aa7cd08
ansushina <nastya.sushina@yandex.ru>
2019-03-03 15:23:40 +0300

    changes in alg3

--- alg/lab1/alg3/main.cpp
+++ alg/lab1/alg3/main.cpp
@@ -1,9 +1,29 @@
-#include <iostream>
+//#include <iostream>
 
-using namespace std;
+#include <stdio.h>
+
+//using namespace std;
+
+class Deque
+{
+
+public:
+    Deque();
+    ~Deque;
+    void pushBack();
+    void pushFront();
+    int popBack();
+    int popFront();
+    bool IsEmpty() const;
+
+private:
+    int *head, *tail;
+    int *mas;
+    size_t size;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //cout << "Hello World!" << endl;
     return 0;
 }
